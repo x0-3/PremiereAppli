e9c49ca4053647e601d3e3da349059c78e0b973b
x0-3 <steph6363@gmail.com>
2023-01-05 12:02:39 +0100

    fixed layout in table

--- recap.php
+++ recap.php
@@ -15,10 +15,9 @@ session_start();// start session
 <body>
 
     <?php 
-    var_dump($_SESSION);
     // if there is no products in the session than write
     if(!isset($_SESSION['products']) || empty($_SESSION['products'])){
-        echo "<p>Aucun produit en session</p>";
+        echo "<p>Aucun produit en session...</p>";
     }else{
         // create an html table that will contain our products
         echo "<table>", 
@@ -31,7 +30,7 @@ session_start();// start session
                         "<th>Total</th>",  
                     "</tr>",
                 "</thead>",
-                "</tbody>";
+                "<tbody>";
         
         // execute product by product the same instructions that will show inside the table
         $totalGeneral = 0;
@@ -39,19 +38,17 @@ session_start();// start session
 
             // write products info inside a table
             echo "<tr>",
-                    "<td>" .$index. "<td>",
-                    "<td>" .$product['name']. "<td>",
-                    "<td>" .number_format($product['price'],2, ",", "&nbsp"). "&nbsp;€ <td>",
-                    "<td>" .$$product['price']. "<td>",
-                    "<td>" .$$product['qtt']. "<td>",
-                    "<td>" .number_format($product['total'],2, ",", "&nbsp"). "&nbsp;€ <td>",
-                    "<td>" .$$product['total']. "<td>",
+                    "<td>".$index."</td>",
+                    "<td>".$product['name']."</td>",
+                    "<td>".number_format($product['price'], 2, ",", "&nbsp;")."&nbsp;€</td>",
+                    "<td>".$product['qtt']."</td>",
+                    "<td>".number_format($product['total'], 2, ",", "&nbsp;")."&nbsp;€</td>",
                 "</tr>";
             $totalGeneral+=$product['total'];// add product total to the general total
         }
         echo "<tr>",
                 "<td colspan=4>Total général : </td>",
-                "<td><strong>" .number_format($totalGeneral,2, ",", "&nbsp"). "&nbsp;€ </strong></td>",
+                "<td><strong>".number_format($totalGeneral,2, ",", "&nbsp")."&nbsp;€</strong></td>",
             "</tr>",
             "</tbody>",
             "</table>";
